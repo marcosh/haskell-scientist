dc6a4d14d9c98d4ec5968f5d769eec3ff12e9420
Marco Perone <pasafama@gmail.com>
2020-08-14 16:10:47 +0200

    do not depend explicitly on IO

--- src/Scientist/Scientist.hs
+++ src/Scientist/Scientist.hs
@@ -1,29 +1,30 @@
-module Scientist where
+module Scientist.Scientist where
 
+-- base
 import Data.Maybe (catMaybes)
 import System.Random (randomRIO)
 
--- | An 'Operation' is just a newtype around a function from 'a' to 'b'
-newtype Operation a b = Operation (a -> IO b)
+-- | An 'Operation' is just a newtype around an effectful function from 'a' to 'b'
+newtype Operation m a b = Operation (a -> m b)
 
--- | A 'Trial' is an operation in an experiment which has a specific change of being executed
-data Trial a b = Trial
+-- | A 'Trial' is an operation in an experiment which has a specific chance of being executed
+data Trial m a b = Trial
   -- | the name of the 'Trail' operation
   { _tName      :: String
   -- | the trial operation
-  , _tOperation :: Operation a b
+  , _tOperation :: Operation m a b
   -- | the chance that the trial operation is going to be executed inside an experiment
   , _tChanche   :: Int
   }
 
 -- | An 'Experiment' describes a set of 'Operation's which need to be performed, measured and compared
-data Experiment a b = Experiment
+data Experiment m a b = Experiment
   -- | the name of the 'Experiment'
   { _eName    :: String
   -- | the control 'Operation' of the experiment
-  , _eControl :: Operation a b
+  , _eControl :: Operation m a b
   -- | the trial 'Operation's of the experiment
-  , _eTrials  :: [Trial a b]
+  , _eTrials  :: [Trial m a b]
   -- | the chanche that the 'Experiment' is going to be executed
   , _eChance  :: Int
   }
@@ -39,6 +40,7 @@ data Discrepancy a b = Discrepancy
   -- | the output value of the 'Trial' 'Operation'
   , _dTrialValue   :: b
   }
+  deriving (Eq, Show)
 
 -- | The reported data of the results of an 'Experiment'
 data ExperimentResult a b
@@ -48,8 +50,8 @@ data ExperimentResult a b
   | Discrepancies [Discrepancy a b]
 
 -- | Run a single 'Trial'.
--- Returns `Nothing` if the chance of the 'Trail' is less than the probability
-runTrial :: Int -> a -> b -> Trial a b -> Maybe (IO (Discrepancy a b))
+-- Returns `Nothing` if the chance of the 'Trail' is less than the probability and the 'Trial' is not actually run
+runTrial :: Monad m => Int -> a -> b -> Trial m a b -> Maybe (m (Discrepancy a b))
 runTrial probability input controlOutput (Trial name (Operation f) chance) =
   if probability <= chance
   then do
@@ -59,7 +61,7 @@ runTrial probability input controlOutput (Trial name (Operation f) chance) =
   else Nothing
 
 -- | run the 'Experiment'
-runExperiment :: Experiment a b -> a -> IO (b, ExperimentResult a b)
+runExperiment :: Experiment IO a b -> a -> IO (b, ExperimentResult a b)
 runExperiment (Experiment _ (Operation control) trials chance) a = do
   probability <- randomRIO (0, 100)
   controlOutput <- control a
@@ -67,7 +69,6 @@ runExperiment (Experiment _ (Operation control) trials chance) a = do
         if probability <= chance
         then do
           trialDiscrepancies <- sequence . catMaybes $ runTrial (div (probability * chance) 100) a controlOutput <$> trials
-          -- trialDiscrepancies <- sequence trialDiscrepanciesIO
           pure $ Discrepancies trialDiscrepancies
         else
           pure ExperimentNotRun
