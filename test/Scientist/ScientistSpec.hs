835a2811831aea66dafa0d335d81d2aca95b6f16
Marco Perone <pasafama@gmail.com>
2020-08-14 16:23:58 +0200

    complete testing for runTrial

--- test/Scientist/ScientistSpec.hs
+++ test/Scientist/ScientistSpec.hs
@@ -3,7 +3,7 @@
 module Scientist.ScientistSpec where
 
 -- base
-import Data.Functor.Identity (Identity)
+import Data.Functor.Identity (Identity(..))
 
 -- hspec
 import Test.Hspec
@@ -17,10 +17,16 @@ spec :: Spec
 spec =
   describe "Experiment" $ do
 
-    describe "runTrial" $
+    describe "runTrial" $ do
 
       it "does not run the trial if the probability is higher than the chance" $ property $
         forAll (arbitrary `suchThat` \(probability, chance, _, _, _) -> probability > chance) $
           \(probability, chance, input :: String, controlOutput, trialName) ->
             runTrial probability input controlOutput (Trial trialName (Operation (pure :: a -> Identity a)) chance)
               `shouldBe` Nothing
+
+      it "does run the trial is the probability is lower or equal than the chance" $ property $
+        forAll (arbitrary `suchThat` \(probability, chance, _, _, _) -> probability <= chance) $
+          \(probability, chance, input :: String, controlOutput, trialName) ->
+            runTrial probability input controlOutput (Trial trialName (Operation (pure . reverse :: String -> Identity String)) chance)
+              `shouldBe` Just (Identity $ Discrepancy trialName input controlOutput (reverse input))
